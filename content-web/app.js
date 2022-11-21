1a953fb0ca2dbffa0cc710d44a31557bdd7e2a78
shanrath <shanrath@accepteasy.com>
2022-11-21 13:49:26 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("5f9e3592-c250-4bdb-a7f5-a8d8ef20702f");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
